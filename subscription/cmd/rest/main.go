136b4fa340e6e8fd4f236e6d766ba99f73f84d0c
nahK994 <nkskl6@gmail.com>
2024-12-26 11:33:16 +0600

    initiate subscription service

--- subscription/cmd/rest/main.go
+++ subscription/cmd/rest/main.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"tiny-letter-subscription/pkg/app"
+	"tiny-letter-subscription/pkg/db"
+	"tiny-letter-subscription/pkg/handlers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	config := app.GetConfig()
+	db, err := db.Init(config.DB)
+	if err != nil {
+		log.Fatal(err)
+	}
+	h := handlers.NewHandler(db)
+
+	r := gin.Default()
+	r.POST("/subscribe-publisher", h.HandlerSubscribePublisherPlan)
+	r.POST("/subscribe-subscriber", h.HandlerSubscribeSubscriberPlan)
+
+	addr := fmt.Sprintf("%s:%s", config.App.Domain, config.App.Port)
+	r.Run(addr)
+}
--- subscription/pkg/handlers/handlers.go
+++ subscription/pkg/handlers/handlers.go
@@ -1,1 +1,19 @@
 package handlers
+
+import (
+	"tiny-letter-subscription/pkg/db"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Handler struct {
+	repo *db.Repository
+}
+
+func NewHandler(repo *db.Repository) *Handler {
+	return &Handler{repo: repo}
+}
+
+func (h *Handler) HandlerSubscribePublisherPlan(c *gin.Context) {}
+
+func (h *Handler) HandlerSubscribeSubscriberPlan(c *gin.Context) {}
