880bb54615301afbfe05ce7f0f8f609eda878d4a
nahK994 <nkskl6@gmail.com>
2024-12-29 14:37:19 +0600

    update error messages in subscription service

--- subscription/pkg/handlers/handlers.go
+++ subscription/pkg/handlers/handlers.go
@@ -19,7 +19,7 @@ func (h *Handler) HandlerSubscribePublisher(c *gin.Context) {
 	var req db.PublisherSubscriptionRequest
 
 	if err := c.ShouldBindJSON(&req); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid request"})
+		c.JSON(http.StatusBadRequest, "Bad request")
 		return
 	}
 	if err := req.Validate(); err != nil {
@@ -32,38 +32,38 @@ func (h *Handler) HandlerSubscribePublisher(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"message": "Publisher subscribed successfully"})
+	c.JSON(http.StatusOK, "Publisher subscribed successfully")
 }
 
 func (h *Handler) HandlerSubscribePublication(c *gin.Context) {
 	var req db.SubscriberSubscriptionRequest
 
 	if err := c.ShouldBindJSON(&req); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid request"})
+		c.JSON(http.StatusBadRequest, "Bad request")
 		return
 	}
 	if err := req.Validate(); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, "Bad request")
 		return
 	}
 
 	if err := h.repo.SubscribeSubscriberPlan(req); err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		c.JSON(http.StatusInternalServerError, "Internal server error")
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"message": "Subscriber subscribed successfully"})
+	c.JSON(http.StatusOK, "Subscriber subscribed successfully")
 }
 
 func (h *Handler) HandlerChangeSubscriberSubscriptionPlan(c *gin.Context) {
 	var req db.SubscriberChangePlanRequest
 
 	if err := c.ShouldBindJSON(&req); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid request"})
+		c.JSON(http.StatusBadRequest, "Bad request")
 		return
 	}
 	if err := req.Validate(); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, "Bad request")
 		return
 	}
 
@@ -72,25 +72,25 @@ func (h *Handler) HandlerChangeSubscriberSubscriptionPlan(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"message": "Subscriber subscription plan changed successfully"})
+	c.JSON(http.StatusOK, "Subscriber subscription plan changed successfully")
 }
 
 func (h *Handler) HandlerChangePublisherSubscriptionPlan(c *gin.Context) {
 	var req db.PublisherChangePlanRequest
 
 	if err := c.ShouldBindJSON(&req); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid request"})
+		c.JSON(http.StatusBadRequest, "Bad request")
 		return
 	}
 	if err := req.Validate(); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, "Bad request")
 		return
 	}
 
 	if err := h.repo.ChangePublisherSubscriptionPlan(req); err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		c.JSON(http.StatusInternalServerError, "Internal server error")
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"message": "Publisher subscription plan changed successfully"})
+	c.JSON(http.StatusOK, "Publisher subscription plan changed successfully")
 }
