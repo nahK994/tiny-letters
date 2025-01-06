2d9d8eaac9c19ce08e73fb7a1d151665ff8168f8
nahK994 <nkskl6@gmail.com>
2025-01-06 09:10:06 +0600

    create app config for orchestrator

--- orchestrator/pkg/app/config.go
+++ orchestrator/pkg/app/config.go
@@ -1,1 +1,28 @@
 package app
+
+const localhost = "localhost"
+
+type Service struct {
+	Port   string
+	Domain string
+}
+
+type Config struct {
+	Subscription Service
+	Auth         Service
+}
+
+var appConfig Config = Config{
+	Subscription: Service{
+		Port:   "8001",
+		Domain: localhost,
+	},
+	Auth: Service{
+		Port:   "8000",
+		Domain: localhost,
+	},
+}
+
+func GetConfig() Config {
+	return appConfig
+}
