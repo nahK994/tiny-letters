333dd33ede40dd787408e9a2872189cfc8909a4b
nahK994 <nkskl6@gmail.com>
2025-01-22 03:16:59 +0600

    update db tables and create store functions to handle grpc calls in auth service

--- auth/pkg/db/init.go
+++ auth/pkg/db/init.go
@@ -23,7 +23,6 @@ func createTables(db *sql.DB) error {
 		name VARCHAR(20) NOT NULL,
 		email VARCHAR(50) NOT NULL UNIQUE,
 		password VARCHAR(255) NOT NULL,
-		subscription_id INT NOT NULL
 	);
 	`
 
@@ -42,70 +41,32 @@ func createTables(db *sql.DB) error {
 		CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE
 	);
 
-	-- Indexes for faster lookups
 	CREATE INDEX IF NOT EXISTS idx_user_id ON user_roles(user_id);
 	CREATE INDEX IF NOT EXISTS idx_role_id ON user_roles(role_id);
 	`
 
-	createSubscriptionPlanTable := `
-	CREATE TABLE IF NOT EXISTS subscription_plans (
+	createPublisherSubscriptionsTable := `
+	CREATE TABLE IF NOT EXISTS publisher_subscriptions (
 		id SERIAL PRIMARY KEY,
-		name VARCHAR(20) NOT NULL,
-		order INT NOT NULL
+		user_id INT NOT NULL UNIQUE,
+		plan_id INT NOT NULL,
+		CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 	);
+
+	CREATE INDEX IF NOT EXISTS idx_user_id ON publisher_subscriptions(user_id);
 	`
 
-	createPermissionTable := `
-	CREATE TABLE IF NOT EXISTS permissions (
-		id SERIAL PRIMARY KEY,
-		name VARCHAR(20) NOT NULL,
-		subscription_id INT NOT NULL,
-		CONSTRAINT fk_permissions_subscription FOREIGN KEY (subscription_id) REFERENCES subscription_plans(id) ON DELETE CASCADE
-	);
-	CREATE INDEX IF NOT EXISTS idx_permissions_subscription_id ON permissions(subscription_id);
-	`
-
-	createAudienceTable := `
-	CREATE TABLE IF NOT EXISTS plan_audiences (
-		id SERIAL PRIMARY KEY,
-		subscription_id INT NOT NULL,
-		size INT NOT NULL,
-		CONSTRAINT fk_plan_audiences_subscription FOREIGN KEY (subscription_id) REFERENCES subscription_plans(id) ON DELETE CASCADE
-	);
-	CREATE INDEX IF NOT EXISTS idx_plan_audiences_subscription_id ON plan_audiences(subscription_id);
-	`
-
-	createPublisherSubscriptionManagementTable := `
-	CREATE TABLE IF NOT EXISTS publisher_subscription_managements (
+	createSubscriberSubscribersTable := `
+	CREATE TABLE IF NOT EXISTS subscriber_subscriptions (
 		id SERIAL PRIMARY KEY,
 		user_id INT NOT NULL,
-		subscription_id INT NOT NULL,
-		CONSTRAINT fk_publisher_subscription_managements_subscription FOREIGN KEY (subscription_id) REFERENCES subscription_plans(id) ON DELETE CASCADE
-	);
-	CREATE INDEX IF NOT EXISTS idx_publisher_subscription_management_subscription_id ON publisher_subscription_managements(subscription_id);
-	`
-
-	createSubscriberSubscriptionManagementTable := `
-	CREATE TABLE IF NOT EXISTS subscriber_subscription_managements (
-		id SERIAL PRIMARY KEY,
-		user_id INT NOT NULL,
-		subscription_id INT NOT NULL,
 		publication_id INT NOT NULL,
-		CONSTRAINT fk_subscriber_subscription_managements_subscription FOREIGN KEY (subscription_id) REFERENCES subscription_plans(id) ON DELETE CASCADE
+		is_premium BOOLEAN NOT NULL,
 	);
-	CREATE INDEX IF NOT EXISTS idx_subscriber_subscription_management_subscription_id ON subscriber_subscription_managements(subscription_id);
-	CREATE INDEX IF NOT EXISTS idx_subscriber_subscription_management_publication_id ON subscriber_subscription_managements(publication_id);
+	
+	CREATE INDEX IF NOT EXISTS idx_user_id ON subscriber_subscriptions(user_id);
 	`
 
-	if _, err := db.Exec(createSubscriptionPlanTable); err != nil {
-		return fmt.Errorf("failed to create 'subscription_plans' table: %w", err)
-	}
-	if _, err := db.Exec(createPermissionTable); err != nil {
-		return fmt.Errorf("failed to create 'permissions' table: %w", err)
-	}
-	if _, err := db.Exec(createAudienceTable); err != nil {
-		return fmt.Errorf("failed to create 'plan_audiences' table: %w", err)
-	}
 	if _, err := db.Exec(createUserTable); err != nil {
 		return fmt.Errorf("failed to create 'users' table: %w", err)
 	}
@@ -115,11 +76,11 @@ func createTables(db *sql.DB) error {
 	if _, err := db.Exec(userRoleTable); err != nil {
 		return fmt.Errorf("failed to create 'user_roles' table: %w", err)
 	}
-	if _, err := db.Exec(createPublisherSubscriptionManagementTable); err != nil {
-		return fmt.Errorf("failed to create 'publisher_subscription_managements' table: %w", err)
+	if _, err := db.Exec(createPublisherSubscriptionsTable); err != nil {
+		return fmt.Errorf("failed to create 'publisher_subscriptions' table: %w", err)
 	}
-	if _, err := db.Exec(createSubscriberSubscriptionManagementTable); err != nil {
-		return fmt.Errorf("failed to create 'subscriber_subscription_managements' table: %w", err)
+	if _, err := db.Exec(createSubscriberSubscribersTable); err != nil {
+		return fmt.Errorf("failed to create 'subscriber_subscribers' table: %w", err)
 	}
 
 	return nil
--- auth/pkg/db/store.go
+++ auth/pkg/db/store.go
@@ -1,5 +1,7 @@
 package db
 
+import "fmt"
+
 func (r *Repository) CreateUser(userInfo *CreateUserRequest) (int, error) {
 	var userId int
 	err := r.DB.QueryRow("INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING id", userInfo.Name, userInfo.Email, userInfo.Password).Scan(&userId)
@@ -14,21 +16,99 @@ func (r *Repository) CreateUser(userInfo *CreateUserRequest) (int, error) {
 	return userId, err
 }
 
-func (d *Repository) GetUserInfoByEmail(email string) (*JWT_claim, error) {
+func (r *Repository) GetUserInfoByEmail(email string) (*JWT_claim, error) {
 	var claim JWT_claim
-	err := d.DB.QueryRow(`
-		SELECT 
-			id, 
-			password, 
-			role,
-		FROM 
-			users
-		WHERE 
-			email = $1
+	err := r.DB.QueryRow(`
+		SELECT id, password, role,
+		FROM users
+		WHERE email = $1
 	`, email).Scan(&claim.Id, &claim.Password, &claim.Role)
 
 	if err != nil {
 		return nil, err
 	}
 	return &claim, nil
+}
+
+func (r *Repository) ConfirmPublisherSubscription(data *ConfirmPublisherSubscriptionRequest) error {
+	query := `
+	INSERT INTO publisher_subscriptions (user_id, plan_id)
+	VALUES ($1, $2)
+	`
+	_, err := r.DB.Exec(query, data.UserId, data.PlanId)
+	if err != nil {
+		return fmt.Errorf("failed to subscribe publisher to plan: %w", err)
+	}
+	return nil
+}
+
+func (r *Repository) RevokePublisherSubscription(data *RevokePublisherSubscriptionRequest) error {
+	query := `
+	DELETE FROM publisher_subscriptions WHERE user_id = $1
+	`
+	_, err := r.DB.Exec(query, data.UserId)
+	if err != nil {
+		return fmt.Errorf("failed to revoke publisher subscription: %w", err)
+	}
+	return nil
+}
+
+func (r *Repository) ChangePublisherSubscription(data *ChangePublisherSubscriptionRequest) error {
+	query := `
+	SELECT id, plan_id FROM publisher_subscriptions WHERE user_id = $1
+	`
+	if _, err := r.DB.Exec(query, data.UserId); err != nil {
+		return fmt.Errorf("failed to get publisher subscription plan: %w", err)
+	}
+
+	query = `
+	UPDATE publisher_subscriptions SET plan_id = $2 WHERE user_id = $1
+	`
+	if _, err := r.DB.Exec(query, data.UserId, data.ChangedPlanId); err != nil {
+		return fmt.Errorf("failed to change publisher subscription plan: %w", err)
+	}
+
+	return nil
+}
+
+func (r *Repository) JoinPublication(data *JoinPublicationRequest) error {
+	query := `
+	INSERT INTO subscriber_subscriptions (user_id, publication_id, is_premium)
+	VALUES ($1, $2, $3)
+	`
+	if _, err := r.DB.Exec(query, data.UserId, data.PublicationId, data.IsPremium); err != nil {
+		return fmt.Errorf("failed to join publication: %w", err)
+	}
+
+	return nil
+}
+
+func (r *Repository) LeavePublication(data *LeavePublicationRequest) error {
+	query := `
+	DELETE FROM subscriber_subscriptions WHERE user_id = $1 AND publication_id = $2
+	`
+
+	if _, err := r.DB.Exec(query, data.UserId, data.PublicationId); err != nil {
+		return fmt.Errorf("failed to join publication: %w", err)
+	}
+
+	return nil
+}
+
+func (r *Repository) ChangeSubscriberSubscription(data *ChangeSubscriberSubscriptionRequest) error {
+	query := `
+	SELECT is_premium FROM subscriber_subscriptions WHERE user_id = $1 AND publication_id = $2
+	`
+	var isPremium bool
+	if err := r.DB.QueryRow(query, data.UserId, data.PublicationId).Scan(&isPremium); err != nil {
+		return fmt.Errorf("failed to get subscription premium status: %w", err)
+	}
+
+	query = `
+	UPDATE subscriber_subscriptions SET is_premium = $3 WHERE user_id = $1 AND publication_id = $2
+	`
+	if _, err := r.DB.Exec(query, data.UserId, data.PublicationId, !isPremium); err != nil {
+		return fmt.Errorf("failed to update subscription premium status: %w", err)
+	}
+	return nil
 }
