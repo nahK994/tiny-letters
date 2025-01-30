e7a2f4a107a0d5df6ea7520acd0ba531a138dff4
nahK994 <nkskl6@gmail.com>
2025-01-30 15:05:43 +0600

    refactor coordinator

--- coordinator/pkg/app/config.go
+++ coordinator/pkg/app/config.go
@@ -2,33 +2,33 @@ package app
 
 const localhost = "localhost"
 
-type Service struct {
+type CommConfig struct {
 	Port   int
 	Domain string
 }
 
 type GRPC struct {
-	Subscription Service
-	Auth         Service
+	Subscription CommConfig
+	Auth         CommConfig
 }
 
 type Config struct {
 	GRPC
-	REST Service
+	REST CommConfig
 }
 
 var appConfig Config = Config{
 	GRPC: GRPC{
-		Subscription: Service{
+		Subscription: CommConfig{
 			Port:   50002,
 			Domain: localhost,
 		},
-		Auth: Service{
+		Auth: CommConfig{
 			Port:   50000,
 			Domain: localhost,
 		},
 	},
-	REST: Service{
+	REST: CommConfig{
 		Port:   8080,
 		Domain: localhost,
 	},
