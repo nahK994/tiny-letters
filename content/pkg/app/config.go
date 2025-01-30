7bc2c109d9e3f0f9f86aab1a11c003c51361c8c4
nahK994 <nkskl6@gmail.com>
2025-01-30 14:59:48 +0600

    add content app -> config

--- content/pkg/app/config.go
+++ content/pkg/app/config.go
@@ -1,1 +1,60 @@
 package app
+
+const domain = "localhost"
+
+type DBConfig struct {
+	User     string
+	Password string
+	Host     string
+	Name     string
+	Port     int
+}
+
+type AppConfig struct {
+	JWT_secret      string
+	JWT_exp_minutes int
+}
+
+type CommConfig struct {
+	Domain string
+	Port   int
+}
+
+type GRPC_config struct {
+	Subscription CommConfig
+}
+
+type Config struct {
+	App  AppConfig
+	DB   DBConfig
+	REST CommConfig
+	GRPC GRPC_config
+}
+
+var appConfig Config = Config{
+	App: AppConfig{
+		JWT_secret:      "secret",
+		JWT_exp_minutes: 60,
+	},
+	REST: CommConfig{
+		Domain: domain,
+		Port:   8001,
+	},
+	GRPC: GRPC_config{
+		Subscription: CommConfig{
+			Domain: domain,
+			Port:   50002,
+		},
+	},
+	DB: DBConfig{
+		User:     "user",
+		Password: "password",
+		Name:     "content_db",
+		Port:     5001,
+		Host:     domain,
+	},
+}
+
+func GetConfig() Config {
+	return appConfig
+}
