a7e6543111a9fe23b6f8b807438e0c92eedfaeb2
nahK994 <nkskl6@gmail.com>
2025-02-03 21:49:47 +0600

    create producer

---
+++ notification/pkg/mq/producer/producer.go
@@ -0,0 +1,46 @@
+package mq_producer
+
+import (
+	"fmt"
+	"tiny-letter/notification/pkg/app"
+
+	"github.com/IBM/sarama"
+)
+
+type Producer struct {
+	producer sarama.SyncProducer
+}
+
+func NewProducer() (*Producer, error) {
+	producer, err := connectProducer()
+	if err != nil {
+		return nil, err
+	}
+	defer producer.Close()
+	return &Producer{producer: producer}, nil
+}
+
+func connectProducer() (sarama.SyncProducer, error) {
+	appConfig := app.GetConfig()
+	broker := fmt.Sprintf("%s:%d", appConfig.Domain, appConfig.Port)
+
+	mqConfig := sarama.NewConfig()
+	mqConfig.Producer.Return.Successes = appConfig.Producer.IsProducerReturnSuccess
+	mqConfig.Producer.RequiredAcks = sarama.WaitForAll
+	mqConfig.Producer.Retry.Max = appConfig.Producer.NumberOfRetry
+
+	return sarama.NewSyncProducer([]string{broker}, mqConfig)
+}
+
+func (p *Producer) Push(topic string, message []byte) error {
+	msg := &sarama.ProducerMessage{
+		Topic: topic,
+		Value: sarama.StringEncoder(message),
+	}
+	_, _, err := p.producer.SendMessage(msg)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
