a9e7fdce2f5a7d8ed7dd8951d2e4d8d22cc0247b
nahK994 <nkskl6@gmail.com>
2025-02-04 02:56:06 +0600

    add grpc comm with notification

--- subscription/cmd/grpc/server/server.go
+++ subscription/cmd/grpc/server/server.go
@@ -7,9 +7,11 @@ import (
 	"sync"
 	pb_content "tiny-letter/subscription/cmd/grpc/pb/content"
 	pb_coordinator "tiny-letter/subscription/cmd/grpc/pb/coordinator"
+	pb_notification "tiny-letter/subscription/cmd/grpc/pb/notification"
 	"tiny-letter/subscription/pkg/db"
 	content_handlers "tiny-letter/subscription/pkg/handlers/content"
 	coordinator_handlers "tiny-letter/subscription/pkg/handlers/coordinator"
+	notification_handlers "tiny-letter/subscription/pkg/handlers/notification"
 
 	"google.golang.org/grpc"
 )
@@ -24,6 +26,7 @@ func Serve(wg *sync.WaitGroup, db *db.Repository, addr string) {
 	s := grpc.NewServer()
 	pb_content.RegisterContentListenerServer(s, content_handlers.GetContentHandlers(db))
 	pb_coordinator.RegisterCoordinatorListenerServer(s, coordinator_handlers.GetCoordinatorHandlers(db))
+	pb_notification.RegisterNotificationListenerServer(s, notification_handlers.GetNotificationHandlers(db))
 
 	fmt.Println("Starting server...")
 	fmt.Printf("Hosting server on: %s\n", lis.Addr().String())
