bf4d17bfdd414173c903ab48d244ff0cd92b82d7
nahK994 <nkskl6@gmail.com>
2025-02-03 21:50:43 +0600

    initiate handlers in notification service

--- notification/pkg/handlers/handlers.go
+++ notification/pkg/handlers/handlers.go
@@ -1,1 +1,21 @@
 package handlers
+
+import mq_producer "tiny-letter/notification/pkg/mq/producer"
+
+type Handler struct {
+	producer *mq_producer.Producer
+}
+
+func NewHandler(producer *mq_producer.Producer) *Handler {
+	return &Handler{
+		producer: producer,
+	}
+}
+
+func (h *Handler) HandleConfirmationMsg(msg []byte) {
+
+}
+
+func (h *Handler) HandlePublicationMsg(msg []byte) {
+
+}
