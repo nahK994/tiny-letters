3b6e5f481a5df1941f312b49a6198515013b85e3
nahK994 <nkskl6@gmail.com>
2025-02-03 21:51:20 +0600

    create consumer in notification service

---
+++ notification/pkg/mq/consumer/consumer.go
@@ -0,0 +1,69 @@
+package mq_consumer
+
+import (
+	"fmt"
+	"tiny-letter/notification/pkg/app"
+	"tiny-letter/notification/pkg/handlers"
+
+	"github.com/IBM/sarama"
+)
+
+type (
+	ConfirmationConsumer sarama.PartitionConsumer
+	PublicationConsumer  sarama.PartitionConsumer
+)
+
+type Consumer struct {
+	confirmationConsumer sarama.PartitionConsumer
+	publicationConsumer  sarama.PartitionConsumer
+	handler              *handlers.Handler
+}
+
+func NewConsumer(handler *handlers.Handler) (*Consumer, error) {
+	confirmationConsumer, publicationConsumer, err := ConnectConsumer()
+	if err != nil {
+		return nil, err
+	}
+
+	return &Consumer{
+		confirmationConsumer: confirmationConsumer,
+		publicationConsumer:  publicationConsumer,
+		handler:              handler,
+	}, nil
+}
+
+func ConnectConsumer() (ConfirmationConsumer, PublicationConsumer, error) {
+	appConfig := app.GetConfig()
+	broker := fmt.Sprintf("%s:%d", appConfig.Domain, appConfig.Port)
+	mqConfig := sarama.NewConfig()
+	mqConfig.Consumer.Return.Errors = appConfig.Consumer.IsConsumerReturnError
+	worker, err := sarama.NewConsumer([]string{broker}, mqConfig)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to start worker")
+	}
+
+	confirmationConsumer, err := worker.ConsumePartition(appConfig.Consumer.Confirmation.Topic, 0, sarama.OffsetOldest)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to start confirmation consumer: %w", err)
+	}
+	publicationConsumer, err := worker.ConsumePartition(appConfig.Consumer.Publication.Topic, 0, sarama.OffsetOldest)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to start publication consumer: %w", err)
+	}
+	return confirmationConsumer, publicationConsumer, nil
+}
+
+func (consumer *Consumer) StartConsuming() {
+	for {
+		select {
+		case err := <-consumer.confirmationConsumer.Errors():
+			fmt.Println(err)
+		case msg := <-consumer.confirmationConsumer.Messages():
+			consumer.handler.HandleConfirmationMsg(msg.Value)
+		case err := <-consumer.publicationConsumer.Errors():
+			fmt.Println(err)
+		case msg := <-consumer.publicationConsumer.Messages():
+			consumer.handler.HandlePublicationMsg(msg.Value)
+		}
+	}
+}
