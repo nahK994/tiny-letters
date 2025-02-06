31c36c13791da2273eb5f9e039a1cb2c6c034a81
nahK994 <nkskl6@gmail.com>
2025-02-06 08:45:37 +0600

    refactor cmd in subscription service

--- subscription/cmd/grpc/server/server.go
+++ subscription/cmd/grpc/server/server.go
@@ -7,15 +7,19 @@ import (
 	"sync"
 	pb_publication_authorization "tiny-letter/subscription/cmd/grpc/pb/publication_authorization"
 	pb_subscription_manager "tiny-letter/subscription/cmd/grpc/pb/subscription_manager"
+	"tiny-letter/subscription/pkg/app"
 	"tiny-letter/subscription/pkg/db"
 	content_handlers "tiny-letter/subscription/pkg/handlers/content"
 	coordinator_handlers "tiny-letter/subscription/pkg/handlers/coordinator"
+	mq_producer "tiny-letter/subscription/pkg/mq/producer"
 
 	"google.golang.org/grpc"
 )
 
-func Serve(wg *sync.WaitGroup, db *db.Repository, addr string) {
+func Serve(wg *sync.WaitGroup, db *db.Repository, config *app.CommConfig, producer *mq_producer.Producer) {
 	defer wg.Done()
+
+	addr := fmt.Sprintf("%s:%d", config.Domain, config.Port)
 	lis, err := net.Listen("tcp", addr)
 	if err != nil {
 		log.Fatalf("Failed to listen: %v", err)
@@ -23,7 +27,7 @@ func Serve(wg *sync.WaitGroup, db *db.Repository, addr string) {
 
 	s := grpc.NewServer()
 	pb_publication_authorization.RegisterPublicationAuthorizationServer(s, content_handlers.GetContentHandlers(db))
-	pb_subscription_manager.RegisterSubscriptionManagerServer(s, coordinator_handlers.GetCoordinatorHandlers(db))
+	pb_subscription_manager.RegisterSubscriptionManagerServer(s, coordinator_handlers.GetCoordinatorHandlers(db, producer))
 
 	fmt.Println("Starting server...")
 	fmt.Printf("Hosting server on: %s\n", lis.Addr().String())
--- subscription/cmd/main.go
+++ subscription/cmd/main.go
@@ -1,24 +1,29 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"sync"
 	grpc_server "tiny-letter/subscription/cmd/grpc/server"
 	"tiny-letter/subscription/pkg/app"
 	"tiny-letter/subscription/pkg/db"
+	mq_producer "tiny-letter/subscription/pkg/mq/producer"
 )
 
 func main() {
 	config := app.GetConfig()
-	addr := fmt.Sprintf("%s:%d", config.GRPC.Domain, config.GRPC.Port)
+
 	db, err := db.Init(config.DB)
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
 	}
 
+	producer, err := mq_producer.NewProducer(&config.MQ)
+	if err != nil {
+		log.Fatalf("Failed to connect to MQ: %v", err)
+	}
+
 	var wg sync.WaitGroup
 	wg.Add(1)
-	go grpc_server.Serve(&wg, db, addr)
+	go grpc_server.Serve(&wg, db, &config.GRPC, producer)
 	wg.Wait()
 }
