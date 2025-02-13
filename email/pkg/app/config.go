d66eb3e96d345e4eb72c9d544c0420365d696e49
nahK994 <nkskl6@gmail.com>
2025-02-13 22:35:27 +0600

    create app -> config for email service

--- email/pkg/app/config.go
+++ email/pkg/app/config.go
@@ -1,1 +1,50 @@
 package app
+
+import "tiny-letter/email/pkg/constants"
+
+type CommConfig struct {
+	Port   int
+	Domain string
+}
+
+type Producer struct {
+	IsProducerReturnSuccess bool
+	NumberOfRetry           int
+}
+
+type MQ_config struct {
+	ConfirmationTopic string
+	PublicationTopic  string
+	CommConfig
+	IsConsumerReturnError bool
+}
+
+type Config struct {
+	DB   CommConfig
+	MQ   MQ_config
+	GRPC CommConfig
+}
+
+var appConfig Config = Config{
+	MQ: MQ_config{
+		ConfirmationTopic: constants.ConfirmationEmail,
+		PublicationTopic:  constants.PublicationEmail,
+		CommConfig: CommConfig{
+			Port:   constants.MQ_port,
+			Domain: constants.Domain,
+		},
+		IsConsumerReturnError: constants.Consumer_IsConsumerReturnError,
+	},
+	GRPC: CommConfig{
+		Domain: constants.Domain,
+		Port:   constants.GRPC_port,
+	},
+	DB: CommConfig{
+		Domain: constants.Domain,
+		Port:   constants.DB_port,
+	},
+}
+
+func GetConfig() Config {
+	return appConfig
+}
---
+++ email/pkg/constants/constants.go
@@ -0,0 +1,14 @@
+package constants
+
+const (
+	ConfirmationEmail = "confirmation_email"
+	PublicationEmail  = "publication_email"
+
+	Domain = "localhost"
+
+	MQ_port                        = 9092
+	Consumer_IsConsumerReturnError = true
+
+	GRPC_port = 50003
+	DB_port   = 5003
+)
