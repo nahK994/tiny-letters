306cda0ec13d378e952eecd45bed0fd8e6af341b
nahK994 <nkskl6@gmail.com>
2025-02-15 23:40:32 +0600

    remove helpers from notification service

--- notification/pkg/handlers/handlers.go
+++ notification/pkg/handlers/handlers.go
@@ -58,3 +58,27 @@ func (h *Handler) HandlePublicationMsg(msgBytes []byte) error {
 	})
 	return h.producer.Push(constants.PublicationEmail, data)
 }
+
+func (h *Handler) handleJoinPublication(msg []byte) error {
+	return h.producer.Push(constants.ConfirmationEmail, msg)
+}
+
+func (h *Handler) handleLeavePublication(msg []byte) error {
+	return h.producer.Push(constants.ConfirmationEmail, msg)
+}
+
+func (h *Handler) handleChangeSubscriberSubscription(msg []byte) error {
+	return h.producer.Push(constants.ConfirmationEmail, msg)
+}
+
+func (h *Handler) handleConfirmPublisherSubscription(msg []byte) error {
+	return h.producer.Push(constants.ConfirmationEmail, msg)
+}
+
+func (h *Handler) handleRevokePublisherSubscription(msg []byte) error {
+	return h.producer.Push(constants.ConfirmationEmail, msg)
+}
+
+func (h *Handler) handleChangePublisherSubscription(msg []byte) error {
+	return h.producer.Push(constants.ConfirmationEmail, msg)
+}
