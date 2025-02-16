188e2d1ba74fdcb493547ce06b6f98cdc802660f
nahK994 <nkskl6@gmail.com>
2025-02-17 00:35:04 +0600

    create CreateUser function in db in email

--- email/pkg/db/store.go
+++ email/pkg/db/store.go
@@ -1,1 +1,12 @@
 package db
+
+import "tiny-letter/email/pkg/models"
+
+func (r *Repository) CreateUser(data models.UserRegistrationRequest) error {
+	_, err := r.DB.Exec("INSERT INTO users (user_id, email) VALUES ($1, $2)", data.UserId, data.Email)
+	if err != nil {
+		return err
+	}
+
+	return err
+}
