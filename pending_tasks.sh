fe16cc5bb9e94cbb626213666190cf249a997c50
nahK994 <nkskl6@gmail.com>
2025-05-01 16:01:50 +0600

    create pending_tasks.sh

---
+++ pending_tasks.sh
@@ -0,0 +1,4 @@
+1. Update user registration process to include a confirmation step.
+2. Implement a password reset feature for users.
+3. Manage user permissions according to subscription levels(e.g., Expired/Unsubscribed users (grace period, content access), Subscription change scenarios).
+4. Publish content.
