eb29fcbe541149b0ca68863816e6f30fc27c1276
nahK994 <nkskl6@gmail.com>
2025-05-02 02:49:14 +0600

    add new pending task

--- pending_tasks.sh
+++ pending_tasks.sh
@@ -1,3 +1,4 @@
 1. Manage user permissions according to subscription levels(e.g., Expired/Unsubscribed users (grace period, content access), Subscription change scenarios).
 2. Publish content.
 3. Create middlewares.
+4. Add migration files
